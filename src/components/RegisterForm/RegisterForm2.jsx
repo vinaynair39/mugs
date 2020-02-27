872a14cf10264aaf16d57c1b2dff14658390585f
ranjeet patil <patilranjeet3699@gmail.com>
2020-02-28 00:39:02 +0530

    Student Registration with Hooks Implemented unresponsive

---
+++ src/components/RegisterForm/RegisterForm2.jsx
@@ -0,0 +1,115 @@
+import React,{useState} from 'react';
+import { Typography,Input,Divider,Menu, Dropdown, Button, Icon, message} from 'antd';
+import './RegisterForm.scss'
+const { Text } = Typography;
+
+/*
+-student details
+    -name
+    -gender
+    -password
+    confirm password
+-college details
+    -college name
+    -roll number
+-contact details
+    -email
+    -phone number
+*/
+
+
+let RegisterForm2=(props)=>{
+    const [name,setName]=useState("");
+    const [gender,setGender]=useState("");
+    const [password,setPassword]=useState("");
+    const [email,setEmail]=useState("");
+    const [number,setNumber]=useState("");
+    const [collegeName,setCollegeName]=useState("Select the college");
+    const [rollno,setRollno]=useState("");
+    
+    const menu = (
+        <Menu onClick={handleMenuClick}>
+        <Menu.Item key="rait">
+            <Icon type="user" />
+            rait
+        </Menu.Item>
+        <Menu.Item key="SIES">
+            <Icon type="user" />
+            SIES
+        </Menu.Item>
+        <Menu.Item key="Pillais">
+            <Icon type="user" />
+            Pillais
+        </Menu.Item>
+        </Menu>
+    );
+    function handleMenuClick(e) {
+        const {key}=e;
+        message.info(`${key} is selected`);
+        setCollegeName(key);
+       // console.log('click', e);
+    }
+
+return(
+    <div className="register">
+        <div className="register_heading flex-bottom"><Text >Student Registration</Text></div><Divider/>
+        <div className="register_student "><div><Text className="side-heading">Student details</Text> </div>
+        <div className="register_student-1">
+            <div className="spacing">
+            <Text>Full Name</Text>
+            <Input placeholder="Full Name" onChange={(e)=>setName(e.target.value)} value={name} />
+            </div> 
+            <div className="spacing">
+            <Text>Gender</Text>
+            <Input placeholder="gender" onChange={(e)=>setGender(e.target.value)} value={gender} />
+            </div>
+            
+            <div className="spacing">
+            <Text>Password</Text>
+            <Input.Password placeholder="input password" onChange={(e)=>setPassword(e.target.value)} value={password} />
+            </div>
+            
+            <div className="spacing">
+            <Text>Confirm Password</Text>
+            <Input.Password placeholder="Confirm password" />
+            </div>
+            
+        </div>
+        </div><Divider/>
+        <div className="register_contact"><div><Text className="side-heading" >Contact Details</Text></div>
+        <div className="register_student-1">
+        <div className="spacing">
+            <Text>Email Address</Text>
+            <Input placeholder="Email address"  onChange={(e)=>setEmail(e.target.value)} value={email}/>
+            </div> 
+            <div className="spacing">
+            <Text>Phone Number</Text>
+            <Input placeholder="Phone Number"  onChange={(e)=>setNumber(e.target.value)} value={number}  />
+            </div>
+        </div>
+    </div><Divider/>
+        <div className="register_college"><div><Text className="side-heading">College Details</Text></div>
+        <div className="register_student-1">
+        <div className="spacing">
+        <Text>Name of the College</Text><br/>
+        <Dropdown overlay={menu}  trigger={['click']} onChange={(e)=>setCollegeName(e.target.value.key)} value={collegeName}>
+        <Button>
+        {collegeName}<Icon type="down" />
+        </Button>
+        </Dropdown>
+        </div>
+        <div className="spacing">
+        <Text>Roll Number</Text>
+        <Input placeholder="Roll Number"  onChange={(e)=>setRollno(e.target.value)} value={rollno} />
+        </div>
+        </div> 
+        </div><Divider/>
+        <div className="actions">
+        <Button size={"large"}>Cancel</Button>
+        <Button type="Primary" size={"large"}>Submit</Button>
+        </div>
+    </div>
+);
+}
+
+export default RegisterForm2
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -7,7 +7,8 @@ import Layout from '../containers/Layout';
 import NavBar from '../containers/NavBar';
 import LoginPage from '../Pages/LoginPage/LoginPage';
 import RegisterPage from '../Pages/RegisterPage.jsx/RegisterPage';
-
+import RegisterForm from '../components/RegisterForm/RegisterForm2';
+import RegisterForm2 from '../components/RegisterForm/RegisterForm2';
 export const history = createHistory();
 
 const AppRouter = () => (
@@ -15,7 +16,7 @@ const AppRouter = () => (
       <Switch>
         <PublicRoute path='/' component={Layout} exact={true} />
         <PublicRoute path='/nav' component={NavBar} exact={true} />
-        <PublicRoute path='/register' component={RegisterPage} exact={true} />
+        <PublicRoute path='/register' component={RegisterForm2} exact={true} />
         <PublicRoute path='/login' component={LoginPage} exact={true} />
         <Redirect from="/" to="/components" />
       </Switch>
